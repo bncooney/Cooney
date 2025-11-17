974238edb0c5d2954b152a8b479a172bbffa2f80
Neal Cooney <github@nealcooney.com>
2025-11-17 22:58:04 +1100

    Init commit

---
+++ AI.Test/ComfyUITests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.AI;
+using System.Reflection;
+
+namespace AI.Test;
+
+[TestClass]
+public sealed class ComfyUITests
+{
+	public TestContext? TestContext { get; set; }
+
+	[TestMethod]
+	public async Task TestMethod1()
+	{
+		IImageGenerator client = new ComfyUIApiClient("http://127.0.0.1:8188");
+
+		var image = await client.GenerateAsync(new ImageGenerationRequest
+		{
+			Prompt = "A beautiful landscape painting of mountains during sunset.",
+		}, cancellationToken: TestContext!.CancellationToken);
+
+		Assert.IsNotNull(image);
+		Assert.IsNotNull(image.RawRepresentation);
+
+		string assemblyName = Assembly.GetExecutingAssembly().GetName().Name!;
+		string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss-fff");
+		string tempDirPath = Path.Combine(Path.GetTempPath(), assemblyName, timestamp);
+		Directory.CreateDirectory(tempDirPath);
+
+		string filePath = Path.Combine(tempDirPath, "generated_image.png");
+		await File.WriteAllBytesAsync(filePath, (byte[])image.RawRepresentation, TestContext.CancellationToken);
+
+		TestContext.AddResultFile(filePath);
+	}
+}
---
+++ AI.Test/OllamaTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+using System.Text;
+
+namespace AI.Test;
+
+[TestClass]
+public sealed class OllamaTests
+{
+	public TestContext? TestContext { get; set; }
+
+	[TestMethod]
+	public async Task TestMethod1()
+	{
+		IChatClient chatClient = new OllamaApiClient(new Uri("http://192.168.1.41:11434/"), "gpt-oss:20b");
+		List<ChatMessage> chatHistory =
+		[
+			new ChatMessage(ChatRole.System, "You are a helpful assistant, answering questions in a concise manner, as quickly as possible."),
+		];
+
+		var response = new StringBuilder();
+		
+		await foreach (ChatResponseUpdate item in chatClient.GetStreamingResponseAsync(chatHistory))
+		{
+			response.Append(item.Text);
+		}
+
+		TestContext?.WriteLine(response.ToString());
+	}
+}
